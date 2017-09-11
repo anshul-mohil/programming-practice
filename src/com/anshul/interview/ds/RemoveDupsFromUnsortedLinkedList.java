a53716c860520c79e94123fba39f5a6bd9a2eb20
Ansh <anshulmohil@hotmail.com>
2017-09-11 22:19:21 +0530

    11 spet work

---
+++ src/com/anshul/interview/ds/RemoveDupsFromUnsortedLinkedList.java
@@ -0,0 +1,63 @@
+package com.anshul.interview.ds;
+
+import java.util.HashSet;
+import com.anshul.interview.ds.LinkedList.Node;
+public class RemoveDupsFromUnsortedLinkedList {
+	/**
+	 * Remove duplicate using HashSet on unsorted LinkedList
+	 */
+	private void removeDuplicates(Node head) {
+		if (head == null)
+			return;
+		HashSet<Integer> hs = new HashSet<Integer>();
+		Node curr = head.next, prev = head;
+		hs.add(prev.data);
+		while (curr != null) {
+			if (hs.contains(curr.data)) {
+				prev.next = curr.next;
+			} else {
+				hs.add(curr.data);
+				prev = prev.next;
+			}
+			curr = curr.next;
+		}
+	}
+/*
+	 Drier program to test above functions 
+	public static void main(String args[]) {
+		LinkedList llist = new LinkedList();
+		llist.push(20);
+		llist.push(13);
+		llist.push(13);
+		llist.push(11);
+		llist.push(11);
+		llist.push(11);
+
+		System.out.println("List before removal of duplicates");
+		llist.printList();
+
+		new RemoveDupsFromUnsortedLinkedList().removeDuplicates(llist.head);
+
+		System.out.println("List after removal of elements");
+		llist.printList();
+	}*/
+	
+	   public static void main(String[] args) {
+	        LinkedList list = new LinkedList();
+	        list.head = new Node(10);
+	        list.head.next = new Node(12);
+	        list.head.next.next = new Node(11);
+	        list.head.next.next.next = new Node(11);
+	        list.head.next.next.next.next = new Node(10);
+	        list.head.next.next.next.next.next = new Node(11);
+	        list.head.next.next.next.next.next.next = new Node(10);
+	 
+	        System.out.println("Linked List before removing duplicates : \n ");
+	        list.printList();
+	 
+	        new RemoveDupsFromUnsortedLinkedList().removeDuplicates(list.head);
+	        System.out.println("");
+	        System.out.println("Linked List after removing duplicates : \n ");
+	        list.printList();
+	    }
+}
