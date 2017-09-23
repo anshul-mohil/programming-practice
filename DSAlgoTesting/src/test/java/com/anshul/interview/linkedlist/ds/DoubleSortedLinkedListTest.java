20ce6cc97c6a5b087a05699dcb21ee9a858bf3d6
Ansh <anshulmohil@hotmail.com>
2017-09-23 14:17:15 +0530

    migrating to multi module approach

---
+++ DSAlgoTesting/src/test/java/com/anshul/interview/linkedlist/ds/DoubleSortedLinkedListTest.java
@@ -0,0 +1,20 @@
+package com.anshul.interview.linkedlist.ds;
+
+import org.w3c.dom.Node;
+
+public class DoubleSortedLinkedListTest {
+//	public static void setup(String[] args) {
+//	DoubleSortedLinkedList object = new DoubleSortedLinkedList();
+//	Node newNode = object.sortedInsert(null, 21);
+//	newNode = object.sortedInsert(newNode, 20);
+//	newNode = object.sortedInsert(newNode, 32);
+//	newNode = object.sortedInsert(newNode, 19);
+//	newNode = object.sortedInsert(newNode, 41);
+//	newNode = object.sortedInsert(newNode, 1);
+//	System.out.println(" Hello");
+//	while(newNode != null) {
+//		System.out.println(newNode.data + " -- ");
+//		newNode = newNode.next;
+//	}
+//}
+}
