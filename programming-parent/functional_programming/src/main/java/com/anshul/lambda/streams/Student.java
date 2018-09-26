8100bd5d85499c6b20dd27b59dc5fb63e5a77179
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-26 20:36:50 +0530

    Student pojo

--- programming-parent/functional_programming/src/main/java/com/anshul/lambda/streams/Student.java
+++ programming-parent/functional_programming/src/main/java/com/anshul/lambda/streams/Student.java
@@ -1,4 +1,22 @@
 package com.anshul.lambda.streams;
 
 public class Student {
+  String name;
+  int id;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
