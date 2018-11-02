6ab4cc4dda2bc6706efc72adcbf972bfc1718f6e
Anshul Mohil <anshulmohil@hotmail.com>
2018-11-02 23:10:02 +0530

    project build related

---
+++ programming-parent/datastructure_algo/src/main/java/com/anshul/interview/asked/ScrachFile.java
@@ -0,0 +1,15 @@
+package com.anshul.interview.asked;
+
+import java.io.IOException;
+
+public class ScrachFile {
+  public static void main(String[] args) throws IOException {
+
+    //language=JSON5
+    String string = "{\"firstName\": \"anshul\",\n" +
+        "\"secondNam\": \"Mohil\",\n" +
+        "}";
+
+  }
+
+}
